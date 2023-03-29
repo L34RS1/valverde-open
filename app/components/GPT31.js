b28edf240f5a1b28a4dabdd8882e84176e25c490
isræl <israelvalverdec@gmail.com>
2023-03-29 14:08:47 -0700

    fix api

--- app/components/GPT31.js
+++ app/components/GPT31.js
@@ -49,7 +49,7 @@ import { motion, AnimatePresence } from "framer-motion"
     }, {
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        Authorization: `Bearer ${API_KEY}`,
       },
     });
 
